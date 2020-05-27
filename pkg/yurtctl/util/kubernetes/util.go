3f81b5c7a85d4a570516d607a3b6013d911bd063
chao zheng <charleszheng44@gmail.com>
2020-05-26 21:10:15 -0700

    1. implement revert subcommand, which allows user to revert a yurt cluster back to a kubernetes cluster 2. extract common functions and put them in pkg/yurtctl/util/kubernetes/util.go 3. fix golint issues 4. add yurtctl tutorial draft docs/tutorial/yurtctl.md 5. add docs/todo.md

--- pkg/yurtctl/util/kubernetes/util.go
+++ pkg/yurtctl/util/kubernetes/util.go
@@ -2,6 +2,8 @@ package kubernetes
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 	"time"
 
 	batchv1 "k8s.io/api/batch/v1"
@@ -12,13 +14,23 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/klog"
+
+	"github.com/alibaba/openyurt/pkg/yurtctl/constants"
+	tmplutil "github.com/alibaba/openyurt/pkg/yurtctl/util/templates"
+)
+
+const (
+	ConvertJobNameBase = "yurtctl-servant-convert"
+	RevertJobNameBase  = "yurtctl-servant-revert"
 )
 
 var (
-	propagationPolicy = metav1.DeletePropagationForeground
+	PropagationPolicy     = metav1.DeletePropagationForeground
+	WaitServantJobTimeout = time.Minute * 2
+	CheckServantJobPeriod = time.Second * 10
 )
 
-// YamlToObj deserialize object in yaml format to a runtime.Object
+// YamlToObject deserializes object in yaml format to a runtime.Object
 func YamlToObject(yamlContent []byte) (runtime.Object, error) {
 	decode := serializer.NewCodecFactory(scheme.Scheme).UniversalDeserializer().Decode
 	obj, _, err := decode(yamlContent, nil, nil)
@@ -28,7 +40,7 @@ func YamlToObject(yamlContent []byte) (runtime.Object, error) {
 	return obj, nil
 }
 
-// LableNode add a new label (<key>=<val>) to the given node
+// LabelNode add a new label (<key>=<val>) to the given node
 func LabelNode(cliSet *kubernetes.Clientset, node *v1.Node, key, val string) (*v1.Node, error) {
 	node.Labels[key] = val
 	newNode, err := cliSet.CoreV1().Nodes().Update(node)
@@ -70,7 +82,7 @@ func RunJobAndCleanup(cliSet *kubernetes.Clientset, job *batchv1.Job, timeout, p
 			if job.Status.Succeeded == *job.Spec.Completions {
 				if err := cliSet.BatchV1().Jobs(job.GetNamespace()).
 					Delete(job.GetName(), &metav1.DeleteOptions{
-						PropagationPolicy: &propagationPolicy,
+						PropagationPolicy: &PropagationPolicy,
 					}); err != nil {
 					klog.Errorf("fail to delete succeeded servant job(%s): %s",
 						job.GetName(), err)
@@ -78,9 +90,54 @@ func RunJobAndCleanup(cliSet *kubernetes.Clientset, job *batchv1.Job, timeout, p
 				}
 				return nil
 			}
-			klog.Infof("job(%s) is not succeeded yet: Succeeded(%d), Completions(%d)",
-				job.GetName(), job.Status.Succeeded, *job.Spec.Completions)
 			continue
 		}
 	}
+}
+
+// RunServantJobs launchs servant jobs on specified edge nodes
+func RunServantJobs(cliSet *kubernetes.Clientset, tmplCtx map[string]string, edgeNodeNames []string) error {
+	var wg sync.WaitGroup
+	for _, nodeName := range edgeNodeNames {
+		action, exist := tmplCtx["action"]
+		if !exist {
+			return errors.New("action is not specified")
+		}
+
+		switch action {
+		case "convert":
+			tmplCtx["jobName"] = ConvertJobNameBase + "-" + nodeName
+		case "revert":
+			tmplCtx["jobName"] = RevertJobNameBase + "-" + nodeName
+		default:
+			return fmt.Errorf("unknown action: %s", action)
+		}
+		tmplCtx["nodeName"] = nodeName
+
+		jobYaml, err := tmplutil.SubsituteTemplate(constants.ServantJobTemplate, tmplCtx)
+		if err != nil {
+			return err
+		}
+		srvJobObj, err := YamlToObject([]byte(jobYaml))
+		if err != nil {
+			return err
+		}
+		srvJob, ok := srvJobObj.(*batchv1.Job)
+		if !ok {
+			return errors.New("fail to assert yurtctl-servant job")
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := RunJobAndCleanup(cliSet, srvJob,
+				WaitServantJobTimeout, CheckServantJobPeriod); err != nil {
+				klog.Errorf("fail to run servant job(%s): %s",
+					srvJob.GetName(), err)
+			} else {
+				klog.Infof("servant job(%s) has succeeded", srvJob.GetName())
+			}
+		}()
+	}
+	wg.Wait()
+	return nil
 }
