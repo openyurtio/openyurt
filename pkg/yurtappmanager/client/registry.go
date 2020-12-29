2884ce5ddb020f7d9a4bb87563622f9520270ced
kadisi <iamkadisi@163.com>
2020-12-29 12:02:31 +0800

    add ud controller and webhook
    Signed-off-by: kadisi <iamkadisi@163.com>

---
+++ pkg/yurtappmanager/client/registry.go
@@ -0,0 +1,24 @@
+package client
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var (
+	genericClient *GenericClientset
+)
+
+// NewRegistry creates clientset by client-go
+func NewRegistry(mgr manager.Manager) error {
+	var err error
+	genericClient, err = newForConfig(mgr.GetConfig())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetGenericClient returns clientset
+func GetGenericClient() *GenericClientset {
+	return genericClient
+}
