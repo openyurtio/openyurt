fad779e219db0f77390846acf380d36e557c063f
Albert Lu <498105432@qq.com>
2021-05-10 13:48:10 +0800

    Add TestGetClusterIPDNSandIP and TestGetNodePortDNSandIP unit test (#211)
    * Add TestGetClusterIPDNSandIP and TestGetNodePortDNSandIP unit test
    
    * Add TestGetClusterIPDNSandIP and TestGetNodePortDNSandIP unit test

--- pkg/yurttunnel/server/serveraddr/addr_test.go
+++ pkg/yurttunnel/server/serveraddr/addr_test.go
@@ -17,7 +17,14 @@ package serveraddr
 
 import (
 	"fmt"
+	"net"
+	"reflect"
 	"testing"
+
+	"github.com/openyurtio/openyurt/pkg/yurttunnel/constants"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestGetDefaultDomainsForSvcInputParamEmptyChar(t *testing.T) {
@@ -49,4 +56,215 @@ func TestGetDefaultDomainsForSvc(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestGetNodePortDNSandIP(t *testing.T) {
+	type ExpectValue struct {
+		ErrorMsg string
+		dnsNames []string
+		ips      []net.IP
+	}
+
+	tests := []struct {
+		desc        string
+		nodelist    v1.NodeList
+		expectValue ExpectValue
+	}{
+		{
+			desc:     "there is no cloud node",
+			nodelist: v1.NodeList{},
+			expectValue: ExpectValue{
+				ErrorMsg: "there is no cloud node",
+			},
+		},
+		{
+			desc: "there is no cloud node",
+			nodelist: v1.NodeList{
+				Items: []v1.Node{
+					{
+						Status: v1.NodeStatus{
+							Addresses: []v1.NodeAddress{
+								{
+									Type:    v1.NodeInternalDNS,
+									Address: "192.168.1.1",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectValue: ExpectValue{
+				ErrorMsg: "can't find node IP",
+			},
+		},
+		{
+			desc: "Get IPs Error",
+			nodelist: v1.NodeList{
+				Items: []v1.Node{
+					{
+						Status: v1.NodeStatus{
+							Addresses: []v1.NodeAddress{
+								{
+									Type:    v1.NodeInternalIP,
+									Address: "192.168.1.1",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectValue: ExpectValue{
+				ErrorMsg: "Get IPs Error",
+				ips: []net.IP{
+					net.ParseIP("192.168.1.1"),
+				},
+			},
+		},
+		{
+			desc: "Many IPs",
+			nodelist: v1.NodeList{
+				Items: []v1.Node{
+					{
+						Status: v1.NodeStatus{
+							Addresses: []v1.NodeAddress{
+								{
+									Type:    v1.NodeInternalIP,
+									Address: "192.168.1.1",
+								},
+								{
+									Type:    v1.NodeHostName,
+									Address: "192.168.1.2",
+								},
+								{
+									Type:    v1.NodeExternalDNS,
+									Address: "192.168.1.3",
+								},
+								{
+									Type:    v1.NodeInternalIP,
+									Address: "192.168.1.4",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectValue: ExpectValue{
+				ErrorMsg: "IPs is Invalid.",
+				ips: []net.IP{
+					net.ParseIP("192.168.1.1"),
+					net.ParseIP("192.168.1.4"),
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			//getNodePortDNSandIP(nodeLst *v1.NodeList) ([]string, []net.IP, error)
+			_, getIPS, error := getNodePortDNSandIP(&tt.nodelist)
+			if error != nil {
+				if error.Error() != tt.expectValue.ErrorMsg {
+					t.Errorf("error is %s\n", tt.expectValue.ErrorMsg)
+				}
+			} else {
+				if !reflect.DeepEqual(getIPS, tt.expectValue.ips) {
+					t.Error(tt.expectValue.ErrorMsg)
+				}
+			}
+		})
+	}
+
+}
+
+func TestGetClusterIPDNSandIPForIP(t *testing.T) {
+	type ExpectValue struct {
+		ErrorMsg string
+		dnsNames []string
+		ips      []net.IP
+	}
+
+	tests := []struct {
+		desc        string
+		svc         corev1.Service
+		expectValue ExpectValue
+	}{
+		{
+			desc: "there is constants.YurttunnelServerExternalAddrKey ips is  192.168.1.2",
+			svc: corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						constants.YurttunnelServerExternalAddrKey: "192.168.1.2:8080",
+					},
+				},
+			},
+			expectValue: ExpectValue{
+				ErrorMsg: "there is constants.YurttunnelServerExternalAddrKey ips is  192.168.1.2",
+				ips: []net.IP{
+					net.ParseIP("192.168.1.2"),
+				},
+				dnsNames: []string{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			dnsNames, getIPS, err := getClusterIPDNSandIP(&tt.svc)
+			if err != nil {
+				t.Errorf("error is %v\n", err)
+			} else {
+				if !reflect.DeepEqual(dnsNames, tt.expectValue.dnsNames) ||
+					!reflect.DeepEqual(getIPS, tt.expectValue.ips) {
+					t.Error(tt.expectValue.ErrorMsg)
+				}
+			}
+		})
+	}
+
+}
+func TestGetClusterIPDNSandIPForIPDNS(t *testing.T) {
+	type ExpectValue struct {
+		ErrorMsg string
+		dnsNames []string
+		ips      []net.IP
+	}
+
+	tests := []struct {
+		desc        string
+		svc         corev1.Service
+		expectValue ExpectValue
+	}{
+		{
+			desc: "there is constants.YurttunnelServerExternalAddrKey dnsnames is  www.bing.com:80",
+			svc: corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						constants.YurttunnelServerExternalAddrKey: "www.bing.com:80",
+					},
+				},
+			},
+			expectValue: ExpectValue{
+				ErrorMsg: "there is constants.YurttunnelServerExternalAddrKey dnsnames is  www.bing.com:80",
+				dnsNames: []string{
+					"www.bing.com",
+				},
+				ips: []net.IP{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			dnsNames, getIPS, err := getClusterIPDNSandIP(&tt.svc)
+			if err != nil {
+				t.Errorf("error is %v\n", err)
+			} else {
+				if !reflect.DeepEqual(dnsNames, tt.expectValue.dnsNames) ||
+					!reflect.DeepEqual(getIPS, tt.expectValue.ips) {
+					t.Error(tt.expectValue.ErrorMsg)
+				}
+			}
+		})
+	}
+
 }
