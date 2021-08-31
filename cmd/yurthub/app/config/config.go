acaef1bce820f6eed67b05d3c39deb0df73fa0c5
rambohe-ch <linbo.hlb@alibaba-inc.com>
2021-08-31 13:56:32 +0800

    feature: add discard cloud service filter in yurthub

--- cmd/yurthub/app/config/config.go
+++ cmd/yurthub/app/config/config.go
@@ -28,6 +28,7 @@ import (
 	"github.com/openyurtio/openyurt/pkg/projectinfo"
 	"github.com/openyurtio/openyurt/pkg/yurthub/cachemanager"
 	"github.com/openyurtio/openyurt/pkg/yurthub/filter"
+	"github.com/openyurtio/openyurt/pkg/yurthub/filter/discardcloudservice"
 	"github.com/openyurtio/openyurt/pkg/yurthub/filter/masterservice"
 	"github.com/openyurtio/openyurt/pkg/yurthub/filter/servicetopology"
 	"github.com/openyurtio/openyurt/pkg/yurthub/kubernetes/meta"
@@ -208,4 +209,5 @@ func createSharedInformers(proxyAddr string) (informers.SharedInformerFactory, y
 func registerAllFilters(filters *filter.Filters) {
 	servicetopology.Register(filters)
 	masterservice.Register(filters)
+	discardcloudservice.Register(filters)
 }
