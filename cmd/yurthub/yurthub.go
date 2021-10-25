dcf04928d699ed53acbe9d3934354243545203bf
Congrool <chpzhangyifei@qq.com>
2021-10-25 11:42:44 +0800

    automatically clean dummy interface and iptables rule when yurthub is stopped by k8s

--- cmd/yurthub/yurthub.go
+++ cmd/yurthub/yurthub.go
@@ -22,12 +22,12 @@ import (
 	"time"
 
 	"github.com/openyurtio/openyurt/cmd/yurthub/app"
-	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apiserver/pkg/server"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	cmd := app.NewCmdStartYurtHub(wait.NeverStop)
+	cmd := app.NewCmdStartYurtHub(server.SetupSignalHandler())
 	cmd.Flags().AddGoFlagSet(flag.CommandLine)
 	if err := cmd.Execute(); err != nil {
 		panic(err)
