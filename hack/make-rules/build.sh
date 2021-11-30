dbd88e0f3d7ab30e0f05492693b1b4b33dd28047
cuisongliu <cuisongliu@qq.com>
2021-11-30 12:28:33 +0800

    ci(master): fix `ln` in mac os

--- hack/make-rules/build.sh
+++ hack/make-rules/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Copyright 2020 The OpenYurt Authors.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +17,8 @@
 set -x
 
 YURT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)"
-source "${YURT_ROOT}/hack/lib/init.sh" 
+source "${YURT_ROOT}/hack/lib/init.sh"
 
-HOST_PLATFORM=${HOST_PLATFORM:-"$(host_platform)"}
+HOST_PLATFORM=${HOST_PLATFORM:-"$(go env GOOS)/$(go env GOARCH)"}
 
 build_binaries "$@"
