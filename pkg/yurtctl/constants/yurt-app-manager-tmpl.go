43bd22069af2cb795a302dd9c909dabf58c0c379
JameKeal <70508195+JameKeal@users.noreply.github.com>
2022-04-18 15:35:40 +0800

    【FIX】fix cloud node was heterogeneous (#806)
    * add yurt-app-manager yurtappdaemon/yurtingress deploy and revert
    
    * Revert "add yurt-app-manager yurtappdaemon/yurtingress deploy and revert"
    
    This reverts commit ba28364ed6ba1093d05b6dbf40d5772a34e6574a.
    
    * fix bug: when cloud node was heterogeneous, this parameter will be error

--- pkg/yurtctl/constants/yurt-app-manager-tmpl.go
+++ pkg/yurtctl/constants/yurt-app-manager-tmpl.go
@@ -680,7 +680,6 @@ spec:
           readOnly: true
       nodeSelector:
         openyurt.io/is-edge-worker: "false"
-        beta.kubernetes.io/arch: {{.arch}}
         beta.kubernetes.io/os: linux
       priorityClassName: system-node-critical
       terminationGracePeriodSeconds: 10
