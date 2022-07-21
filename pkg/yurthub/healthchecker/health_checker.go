4a5033a2f69661d26b07ee4ca56ea66d0cc92def
Congrool <chpzhangyifei@zju.edu.cn>
2022-07-21 22:46:18 +0800

    fix bug: cannot create lease in storage

--- pkg/yurthub/healthchecker/health_checker.go
+++ pkg/yurthub/healthchecker/health_checker.go
@@ -221,7 +221,13 @@ func (hc *cloudAPIServerHealthChecker) setLastNodeLease(lease *coordinationv1.Le
 	if err != nil {
 		return fmt.Errorf("failed to convert rv string %s of lease %s/%s, %v", lease.ResourceVersion, lease.Namespace, lease.Name, err)
 	}
-	if _, err := hc.sw.Update(leaseKey, lease, rv); err != nil {
+	_, err = hc.sw.Update(leaseKey, lease, rv)
+	if err == storage.ErrStorageNotFound {
+		klog.Infof("find no lease of %s in storage, init a new one", leaseKey.Key())
+		if err := hc.sw.Create(leaseKey, lease); err != nil {
+			return fmt.Errorf("failed to create the lease %s, %v", leaseKey.Key(), err)
+		}
+	} else if err != nil {
 		return fmt.Errorf("failed to update lease %s/%s, %v", lease.Namespace, lease.Name, err)
 	}
 	return nil
