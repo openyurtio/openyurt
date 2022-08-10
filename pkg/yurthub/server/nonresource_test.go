065636291222e69d97ed3536137c663dbe66d14b
Yuxuan <sodawyx@126.com>
2022-08-10 16:33:54 +0800

    fix: update the unit test

--- pkg/yurthub/server/nonresource_test.go
+++ pkg/yurthub/server/nonresource_test.go
@@ -1,129 +1,40 @@
-/**
- * @Author: Yuxuan WU
- * @Description:
- * @File: nonresource_test
- * @Version: 1.0.0
- * @Date: 2022/8/8 17:58
- */
+/*
+Copyright 2020 The OpenYurt Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package server
 
 import (
-	"bytes"
-	"github.com/openyurtio/openyurt/cmd/yurthub/app/config"
-	"github.com/openyurtio/openyurt/pkg/yurthub/certificate/hubself"
-	"github.com/openyurtio/openyurt/pkg/yurthub/healthchecker"
-	"github.com/openyurtio/openyurt/pkg/yurthub/kubernetes/rest"
+	"fmt"
+	"github.com/openyurtio/openyurt/pkg/yurthub/cachemanager"
 	"github.com/openyurtio/openyurt/pkg/yurthub/storage/disk"
-	"k8s.io/client-go/kubernetes"
 	"net/http"
-	"net/url"
+	"net/http/httptest"
 	"os"
-	"path/filepath"
 	"testing"
 )
 
-var (
-	certificatePEM = []byte(`-----BEGIN CERTIFICATE-----
-MIICRzCCAfGgAwIBAgIJALMb7ecMIk3MMA0GCSqGSIb3DQEBCwUAMH4xCzAJBgNV
-BAYTAkdCMQ8wDQYDVQQIDAZMb25kb24xDzANBgNVBAcMBkxvbmRvbjEYMBYGA1UE
-CgwPR2xvYmFsIFNlY3VyaXR5MRYwFAYDVQQLDA1JVCBEZXBhcnRtZW50MRswGQYD
-VQQDDBJ0ZXN0LWNlcnRpZmljYXRlLTAwIBcNMTcwNDI2MjMyNjUyWhgPMjExNzA0
-MDIyMzI2NTJaMH4xCzAJBgNVBAYTAkdCMQ8wDQYDVQQIDAZMb25kb24xDzANBgNV
-BAcMBkxvbmRvbjEYMBYGA1UECgwPR2xvYmFsIFNlY3VyaXR5MRYwFAYDVQQLDA1J
-VCBEZXBhcnRtZW50MRswGQYDVQQDDBJ0ZXN0LWNlcnRpZmljYXRlLTAwXDANBgkq
-hkiG9w0BAQEFAANLADBIAkEAtBMa7NWpv3BVlKTCPGO/LEsguKqWHBtKzweMY2CV
-tAL1rQm913huhxF9w+ai76KQ3MHK5IVnLJjYYA5MzP2H5QIDAQABo1AwTjAdBgNV
-HQ4EFgQU22iy8aWkNSxv0nBxFxerfsvnZVMwHwYDVR0jBBgwFoAU22iy8aWkNSxv
-0nBxFxerfsvnZVMwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAANBAEOefGbV
-NcHxklaW06w6OBYJPwpIhCVozC1qdxGX1dg8VkEKzjOzjgqVD30m59OFmSlBmHsl
-nkVA6wyOSDYBf3o=
------END CERTIFICATE-----`)
-	keyPEM = []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIIBUwIBADANBgkqhkiG9w0BAQEFAASCAT0wggE5AgEAAkEAtBMa7NWpv3BVlKTC
-PGO/LEsguKqWHBtKzweMY2CVtAL1rQm913huhxF9w+ai76KQ3MHK5IVnLJjYYA5M
-zP2H5QIDAQABAkAS9BfXab3OKpK3bIgNNyp+DQJKrZnTJ4Q+OjsqkpXvNltPJosf
-G8GsiKu/vAt4HGqI3eU77NvRI+mL4MnHRmXBAiEA3qM4FAtKSRBbcJzPxxLEUSwg
-XSCcosCktbkXvpYrS30CIQDPDxgqlwDEJQ0uKuHkZI38/SPWWqfUmkecwlbpXABK
-iQIgZX08DA8VfvcA5/Xj1Zjdey9FVY6POLXen6RPiabE97UCICp6eUW7ht+2jjar
-e35EltCRCjoejRHTuN9TC0uCoVipAiAXaJIx/Q47vGwiw6Y8KXsNU6y54gTbOSxX
-54LzHNk/+Q==
------END RSA PRIVATE KEY-----`)
-	yurthubCon = `apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: temp
-    server: https://10.10.10.113:6443
-  name: default-cluster
-contexts:
-- context:
-    cluster: default-cluster
-    namespace: default
-    user: default-auth
-  name: default-context
-current-context: default-context
-kind: Config
-preferences: {}
-users:
-- name: default-auth
-  user:
-    client-certificate: /tmp/pki/yurthub-current.pem
-    client-key: /tmp/pki/yurthub-current.pem
-`
-	testDir = "/tmp/pki/"
-)
+var rootDir = "/tmp/cache-local"
 
 func TestNonResourceInfoCache(t *testing.T) {
-	remoteServers := map[string]int{"https://10.10.10.113:6443": 2}
-	u, _ := url.Parse("https://10.10.10.113:6443")
-	fakeHealthchecker := healthchecker.NewFakeChecker(true, remoteServers)
-	dStorage, err := disk.NewDiskStorage(testDir)
-	defer func() {
-		if err := os.RemoveAll(testDir); err != nil {
-			t.Errorf("Unable to clean up test directory %q: %v", testDir, err)
-		}
-	}()
-
-	// store the kubelet ca file
-	caFile := filepath.Join(testDir, "ca.crt")
-	if err := dStorage.Create("ca.crt", certificatePEM); err != nil {
-		t.Fatalf("Unable to create the file %q: %v", caFile, err)
-	}
-
-	// store the kubelet-pair.pem file
-	pairFile := filepath.Join(testDir, "kubelet-pair.pem")
-	pd := bytes.Join([][]byte{certificatePEM, keyPEM}, []byte("\n"))
-	if err := dStorage.Create("kubelet-pair.pem", pd); err != nil {
-		t.Fatalf("Unable to create the file %q: %v", pairFile, err)
-	}
-
-	// store the yurthub-current.pem
-	yurthubCurrent := filepath.Join(testDir, "yurthub-current.pem")
-	if err := dStorage.Create("yurthub-current.pem", pd); err != nil {
-		t.Fatalf("Unable to create the file %q: %v", yurthubCurrent, err)
-	}
-
-	// set the YurtHubConfiguration
-	cfgNew := &config.YurtHubConfiguration{
-		RootDir:               testDir,
-		RemoteServers:         []*url.URL{u},
-		KubeletRootCAFilePath: caFile,
-		KubeletPairFilePath:   pairFile,
-	}
-
-	cfgNew.CertMgrMode = "hubself"
-	certMgr, err := hubself.NewFakeYurtHubCertManager(testDir, yurthubCon, string(certificatePEM), string(keyPEM))
+	dStorage, err := disk.NewDiskStorage(rootDir)
 	if err != nil {
-		t.Fatalf("error in cfg %v", err)
-	}
-	certMgr.Start()
-	rcm, err := rest.NewRestConfigManager(cfgNew, certMgr, fakeHealthchecker)
-	if err != nil {
-		t.Errorf("failed to create RestConfigManager: %v", err)
+		t.Errorf("disk initialize error: %v", err)
 	}
 
-	rc := rcm.GetRestConfig(true)
-	clientSet, err := kubernetes.NewForConfig(rc)
+	sw := cachemanager.NewStorageWrapper(dStorage)
 
 	testcases := map[string]struct {
 		Accept      string
@@ -131,6 +42,7 @@ func TestNonResourceInfoCache(t *testing.T) {
 		Path        string
 		Code        int
 		ContentType string
+		Response    string
 	}{
 		"version info": {
 			Accept:      "application/json",
@@ -138,6 +50,7 @@ func TestNonResourceInfoCache(t *testing.T) {
 			Path:        "/version",
 			Code:        http.StatusOK,
 			ContentType: "application/json",
+			Response:    "fake-non-resource-info-/version",
 		},
 
 		"discovery v1": {
@@ -146,29 +59,61 @@ func TestNonResourceInfoCache(t *testing.T) {
 			Path:        "/apis/discovery.k8s.io/v1",
 			Code:        http.StatusOK,
 			ContentType: "application/json",
+			Response:    "fake-non-resource-info-/apis/discovery.k8s.io/v1",
 		},
 		"discovery v1beta1": {
 			Accept:      "application/json",
+			Verb:        "GET",
 			Path:        "/apis/discovery.k8s.io/v1beta1",
 			Code:        http.StatusOK,
 			ContentType: "application/json",
+			Response:    "fake-non-resource-info-/apis/discovery.k8s.io/v1beta1",
 		},
 	}
 
 	for k, tt := range testcases {
+		//test for caching the response
 		t.Run(k, func(t *testing.T) {
+			var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				cacheNonResourceInfo(w, req, sw, fmt.Sprintf("non-reosurce-info%s", req.URL.Path), req.URL.Path, true)
+			})
 			req, _ := http.NewRequest(tt.Verb, tt.Path, nil)
 			if len(tt.Accept) != 0 {
 				req.Header.Set("Accept", tt.Accept)
 			}
+			resp := httptest.NewRecorder()
+			handler.ServeHTTP(resp, req)
+			result := resp.Result()
+			if result.StatusCode != tt.Code {
+				t.Errorf("got status code %d, but expect %d", result.StatusCode, tt.Code)
+			}
+			if string(resp.Body.Bytes()) != tt.Response {
+				t.Errorf("got response %s, but expect %s", string(resp.Body.Bytes()), tt.Response)
+			}
 
-			req.RemoteAddr = "127.0.0.1"
+		})
 
+		//test for query the cache
+		t.Run(k, func(t *testing.T) {
 			var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-
+				cacheNonResourceInfo(w, req, sw, fmt.Sprintf("non-reosurce-info%s", req.URL.Path), req.URL.Path, false)
 			})
-			wrapNonResourceHandler(handler, cfgNew, clientSet)
+			req, _ := http.NewRequest(tt.Verb, tt.Path, nil)
+			if len(tt.Accept) != 0 {
+				req.Header.Set("Accept", tt.Accept)
+			}
+			resp := httptest.NewRecorder()
+			handler.ServeHTTP(resp, req)
+			result := resp.Result()
+			if result.StatusCode != tt.Code {
+				t.Errorf("got status code %d, but expect %d", result.StatusCode, tt.Code)
+			}
+			if string(resp.Body.Bytes()) != tt.Response {
+				t.Errorf("got response %s, but expect %s", string(resp.Body.Bytes()), tt.Response)
+			}
 
 		})
 	}
+
+	os.RemoveAll(rootDir)
 }
