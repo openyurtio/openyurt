28ad1c03837b9d10461d9e3dc07f67e275823415
lu chen <1813927768@qq.com>
2022-12-08 10:23:17 +0800

    add poolcoordinator cert manager (#1083)

--- pkg/controller/certificates/csrapprover.go
+++ pkg/controller/certificates/csrapprover.go
@@ -39,6 +39,7 @@ import (
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 
+	poolcoordinatorCert "github.com/openyurtio/openyurt/pkg/controller/poolcoordinator/cert"
 	"github.com/openyurtio/openyurt/pkg/projectinfo"
 	"github.com/openyurtio/openyurt/pkg/yurthub/certificate/token"
 	"github.com/openyurtio/openyurt/pkg/yurttunnel/constants"
@@ -73,6 +74,14 @@ var (
 		{
 			recognize:  isYurtTunnelAgentCert,
 			successMsg: "Auto approving yurt-tunnel-agent client certificate",
+		},
+		{
+			recognize:  isPoolCoordinatorTLSServerCert,
+			successMsg: "Auto approving poolcoordinator-apiserver server certificate",
+		},
+		{
+			recognize:  isPoolCoordinatorClientCert,
+			successMsg: "Auto approving poolcoordinator-apiserver client certificate",
 		},
 	}
 )
@@ -481,6 +490,49 @@ func isYurtTunnelAgentCert(csr *certificatesv1.CertificateSigningRequest, x509cr
 	}
 
 	if x509cr.Subject.CommonName != constants.YurtTunnelAgentCSRCN {
+		return false
+	}
+
+	if !clientRequiredUsages.Equal(usagesToSet(csr.Spec.Usages)) {
+		return false
+	}
+
+	return true
+}
+
+// isPoolCoordinatorTLSServerCert is used to recognize csr from poolcoordinator https server(APIServer, etcdServer)
+func isPoolCoordinatorTLSServerCert(csr *certificatesv1.CertificateSigningRequest, x509cr *x509.CertificateRequest) bool {
+	if csr.Spec.SignerName != certificatesv1.KubeletServingSignerName {
+		return false
+	}
+
+	if len(x509cr.Subject.Organization) != 1 || x509cr.Subject.Organization[0] != user.NodesGroup {
+		return false
+	}
+
+	// at least one of dnsNames or ipAddresses must be specified
+	if len(x509cr.DNSNames) == 0 && len(x509cr.IPAddresses) == 0 {
+		return false
+	}
+
+	if !strings.HasPrefix(x509cr.Subject.CommonName, "system:node:") {
+		return false
+	}
+
+	if !serverRequiredUsages.Equal(usagesToSet(csr.Spec.Usages)) {
+		return false
+	}
+
+	return true
+}
+
+// isYurtTunnelProxyClientCert is used to recognize csr from poolcoordinator client certificate .
+func isPoolCoordinatorClientCert(csr *certificatesv1.CertificateSigningRequest, x509cr *x509.CertificateRequest) bool {
+	if csr.Spec.SignerName != certificatesv1.KubeAPIServerClientSignerName {
+		return false
+	}
+
+	if len(x509cr.Subject.Organization) != 1 || x509cr.Subject.Organization[0] != poolcoordinatorCert.PoolcoordinatorOrg {
 		return false
 	}
 
