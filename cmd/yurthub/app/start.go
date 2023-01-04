2209eb42ff978a2e5b8d7e6ca08d522e8ff4cace
LaurenceLiZhixin <382673304@qq.com>
2023-01-04 17:55:04 +0800

    Fix

--- cmd/yurthub/app/start.go
+++ cmd/yurthub/app/start.go
@@ -274,7 +274,6 @@ func coordinatorRun(ctx context.Context,
 	var coordinatorHealthChecker healthchecker.HealthChecker
 	var coordinatorTransportMgr transport.Interface
 	var coordinator poolcoordinator.Coordinator
-	var coordinatorCertManager *coordinatorcertmgr.CertManager
 	var returnErr error
 
 	readyCh := make(chan struct{})
@@ -289,7 +288,7 @@ func coordinatorRun(ctx context.Context,
 			return
 		}
 
-		coorTransportMgr, err := poolCoordinatorTransportMgrGetter(cfg.HeartbeatTimeoutSeconds, cfg.CoordinatorServerURL, coordinatorCertManager, ctx.Done())
+		coorTransportMgr, err := poolCoordinatorTransportMgrGetter(cfg.HeartbeatTimeoutSeconds, cfg.CoordinatorServerURL, coorCertManager, ctx.Done())
 		if err != nil {
 			returnErr = fmt.Errorf("failed to create coordinator transport manager, %v", err)
 			return
