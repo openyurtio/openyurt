39029b3753b0d852bd5912f04a4e0cdd30a8a390
Yifei Zhang <chpzhangyifei@zju.edu.cn>
2023-01-11 10:06:55 +0800

    bugfix: do not send pool-scoped resource list/watch request to pool-coordinator (#1134)
    Signed-off-by: Congrool <chpzhangyifei@zju.edu.cn>
    
    Signed-off-by: Congrool <chpzhangyifei@zju.edu.cn>

--- pkg/yurthub/proxy/proxy.go
+++ pkg/yurthub/proxy/proxy.go
@@ -37,6 +37,7 @@ import (
 	"github.com/openyurtio/openyurt/pkg/yurthub/cachemanager"
 	"github.com/openyurtio/openyurt/pkg/yurthub/healthchecker"
 	"github.com/openyurtio/openyurt/pkg/yurthub/poolcoordinator"
+	coordinatorconstants "github.com/openyurtio/openyurt/pkg/yurthub/poolcoordinator/constants"
 	"github.com/openyurtio/openyurt/pkg/yurthub/proxy/local"
 	"github.com/openyurtio/openyurt/pkg/yurthub/proxy/pool"
 	"github.com/openyurtio/openyurt/pkg/yurthub/proxy/remote"
@@ -217,6 +218,15 @@ func (p *yurtReverseProxy) eventHandler(rw http.ResponseWriter, req *http.Reques
 }
 
 func (p *yurtReverseProxy) poolScopedResouceHandler(rw http.ResponseWriter, req *http.Request) {
+	agent, ok := hubutil.ClientComponentFrom(req.Context())
+	if ok && agent == coordinatorconstants.DefaultPoolScopedUserAgent {
+		// list/watch request from leader-yurthub
+		// It should always be proxied to cloud APIServer to get latest resource, which will
+		// be cached into pool cache.
+		p.loadBalancer.ServeHTTP(rw, req)
+		return
+	}
+
 	if p.isCoordinatorReady() && p.poolProxy != nil {
 		p.poolProxy.ServeHTTP(rw, req)
 	} else if p.cloudHealthChecker.IsHealthy() {
