66db812dcee6af909169b856c3d488dda1dbdfc4
LaurenceLiZhixin <382673304@qq.com>
2023-01-16 14:59:46 +0800

    Fix: format

--- pkg/yurthub/poolcoordinator/coordinator.go
+++ pkg/yurthub/poolcoordinator/coordinator.go
@@ -24,8 +24,6 @@ import (
 	"sync"
 	"time"
 
-	"k8s.io/klog/v2"
-
 	coordinationv1 "k8s.io/api/coordination/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -40,6 +38,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 
 	"github.com/openyurtio/openyurt/cmd/yurthub/app/config"
 	"github.com/openyurtio/openyurt/pkg/yurthub/cachemanager"
