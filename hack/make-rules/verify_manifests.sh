a65b31c8b52c71bf247483fc43a8fb50dd7b897f
huiwq1990 <huiwq1990@163.com>
2023-04-21 15:55:41 +0800

    feat: crd generate crds (#1389)
    Signed-off-by: huiwq1990 <huiwq1990@163.com>

---
+++ hack/make-rules/verify_manifests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 The OpenYurt Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+set -u
+
+YURT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd -P)"
+
+make manifests
+
+if [ ! -z "$(git diff ${YURT_ROOT}/charts)" ]; then
+    echo "diff"
+    git diff ${YURT_ROOT}/charts
+    echo "Verify manifests error, please run make manifests"
+    exit -1
+fi 
