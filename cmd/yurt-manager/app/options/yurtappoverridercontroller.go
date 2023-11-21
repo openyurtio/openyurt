91dca340f283255fdbe42a6408afa3135f6083af
lilongfeng <lilongfeng@inspur.com>
2023-11-21 16:22:06 +0800

    Comments alse need camelCase format (#1814)
    Signed-off-by: 李龙峰 <lilongfeng@inspur.com>

--- cmd/yurt-manager/app/options/yurtappoverridercontroller.go
+++ cmd/yurt-manager/app/options/yurtappoverridercontroller.go
@@ -32,7 +32,7 @@ func NewYurtAppOverriderControllerOptions() *YurtAppOverriderControllerOptions {
 	}
 }
 
-// AddFlags adds flags related to nodepool for yurt-manager to the specified FlagSet.
+// AddFlags adds flags related to nodePool for yurt-manager to the specified FlagSet.
 func (n *YurtAppOverriderControllerOptions) AddFlags(fs *pflag.FlagSet) {
 	if n == nil {
 		return
@@ -41,7 +41,7 @@ func (n *YurtAppOverriderControllerOptions) AddFlags(fs *pflag.FlagSet) {
 	//fs.BoolVar(&n.CreateDefaultPool, "create-default-pool", n.CreateDefaultPool, "Create default cloud/edge pools if indicated.")
 }
 
-// ApplyTo fills up nodepool config with options.
+// ApplyTo fills up nodePool config with options.
 func (o *YurtAppOverriderControllerOptions) ApplyTo(cfg *config.YurtAppOverriderControllerConfiguration) error {
 	if o == nil {
 		return nil
