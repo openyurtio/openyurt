0f5325ffa9473352eb69539dfd77d367df62dea7
vie-serendipity <2733147505@qq.com>
2024-04-08 14:36:04 +0800

    chore: lint and license

--- pkg/yurtmanager/controller/yurtappset/workloadmanager/statefulset_manager_test.go
+++ pkg/yurtmanager/controller/yurtappset/workloadmanager/statefulset_manager_test.go
@@ -1,15 +1,31 @@
+/*
+Copyright 2024 The OpenYurt Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package workloadmanager
 
 import (
 	"testing"
 
-	"github.com/openyurtio/openyurt/pkg/apis/apps"
-	"github.com/openyurtio/openyurt/pkg/apis/apps/v1beta1"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/openyurtio/openyurt/pkg/apis/apps"
+	"github.com/openyurtio/openyurt/pkg/apis/apps/v1beta1"
 )
 
 var stsYAS = &v1beta1.YurtAppSet{
