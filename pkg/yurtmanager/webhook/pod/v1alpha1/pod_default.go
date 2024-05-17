2154da8290f5f08a0bd85d4bbc441dabada3fab5
rambohe <linbo.hlb@alibaba-inc.com>
2024-05-17 15:36:04 +0800

    improve pod webhook for adapting hostnetwork mode nodepool (#2050)

--- pkg/yurtmanager/webhook/pod/v1alpha1/pod_default.go
+++ pkg/yurtmanager/webhook/pod/v1alpha1/pod_default.go
@@ -27,6 +27,11 @@ import (
 	"github.com/openyurtio/openyurt/pkg/apis/apps"
 )
 
+const (
+	NodePoolHostNetworkLabelKey       = "nodepool.openyurt.io/hostnetwork"
+	NodePoolHostNetworkLabelForbidden = "true"
+)
+
 // Default implements builder.CustomDefaulter.
 func (webhook *PodHandler) Default(ctx context.Context, obj runtime.Object) error {
 	pod, ok := obj.(*corev1.Pod)
@@ -35,34 +40,58 @@ func (webhook *PodHandler) Default(ctx context.Context, obj runtime.Object) erro
 	}
 
 	// Add NodeAffinity to pods in order to avoid pods to be scheduled on the nodes in the hostNetwork mode NodePool
-	annotations := pod.ObjectMeta.GetAnnotations()
-	if annotations != nil && annotations[apps.AnnotationExcludeHostNetworkPool] == "true" {
-		excludeHostNetworkTerm := corev1.NodeSelectorTerm{
-			MatchExpressions: []corev1.NodeSelectorRequirement{
+	if pod.Annotations[apps.AnnotationExcludeHostNetworkPool] != "true" {
+		return nil
+	}
+
+	excludeHostNetworkRequirement := corev1.NodeSelectorRequirement{
+		Key:      NodePoolHostNetworkLabelKey,
+		Operator: corev1.NodeSelectorOpNotIn,
+		Values:   []string{NodePoolHostNetworkLabelForbidden},
+	}
+
+	if pod.Spec.Affinity == nil {
+		pod.Spec.Affinity = &corev1.Affinity{}
+	}
+
+	if pod.Spec.Affinity.NodeAffinity == nil {
+		pod.Spec.Affinity.NodeAffinity = &corev1.NodeAffinity{}
+	}
+
+	if pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution == nil {
+		pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution = &corev1.NodeSelector{
+			NodeSelectorTerms: []corev1.NodeSelectorTerm{
 				{
-					Key:      "nodepool.openyurt.io/hostnetwork",
-					Operator: corev1.NodeSelectorOpNotIn,
-					Values:   []string{"true"},
+					MatchExpressions: []corev1.NodeSelectorRequirement{},
 				},
 			},
 		}
-
-		if pod.Spec.Affinity == nil {
-			pod.Spec.Affinity = &corev1.Affinity{}
-		}
-
-		if pod.Spec.Affinity.NodeAffinity == nil {
-			pod.Spec.Affinity.NodeAffinity = &corev1.NodeAffinity{}
-		}
-
-		if pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution == nil {
-			pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution = &corev1.NodeSelector{}
-		}
-
+	} else if len(pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms) == 0 {
 		pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms = append(
 			pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms,
-			excludeHostNetworkTerm,
-		)
+			corev1.NodeSelectorTerm{
+				MatchExpressions: []corev1.NodeSelectorRequirement{},
+			})
 	}
+
+	for i, term := range pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
+		needToAddAffinity := true
+		for _, expr := range term.MatchExpressions {
+			if expr.Key == NodePoolHostNetworkLabelKey && expr.Operator == corev1.NodeSelectorOpNotIn {
+				if len(expr.Values) == 1 && expr.Values[0] == NodePoolHostNetworkLabelForbidden {
+					needToAddAffinity = false
+					break
+				}
+			}
+		}
+
+		if needToAddAffinity {
+			pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms[i].MatchExpressions = append(
+				pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms[i].MatchExpressions,
+				excludeHostNetworkRequirement,
+			)
+		}
+	}
+
 	return nil
 }
