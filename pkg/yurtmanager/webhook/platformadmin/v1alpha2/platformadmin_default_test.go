612cf6a4ade9829d7cf84a4bfb1f1b255dd8210d
leo9527 <1831131300@qq.com>
2024-08-07 21:21:42 +0800

    add platformadmin ut with tongyilingma

---
+++ pkg/yurtmanager/webhook/platformadmin/v1alpha2/platformadmin_default_test.go
@@ -0,0 +1,119 @@
+package v1alpha2
+
+/*
+Copyright 2023 The OpenYurt Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/openyurtio/openyurt/pkg/apis/iot/v1alpha2"
+	"github.com/openyurtio/openyurt/pkg/yurtmanager/controller/platformadmin/config"
+	"github.com/stretchr/testify/assert"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// TestDefault tests the Default method of PlatformAdminHandler.
+func TestDefaultCompatibleWithV1(t *testing.T) {
+	type testCase struct {
+		name     string
+		obj      runtime.Object
+		expected error
+		version  string
+	}
+
+	tests := []testCase{
+		{
+			name: "should get no error when valid PlatformAdmin object with spec empty version",
+			obj: &v1alpha2.PlatformAdmin{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v1alpha2.PlatformAdminSpec{
+					Version: "",
+				},
+			},
+			expected: nil,
+			version:  "",
+		},
+		{
+			name: "should get no error when valid PlatformAdmin object with spec version is v1",
+			obj: &v1alpha2.PlatformAdmin{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v1alpha2.PlatformAdminSpec{
+					Version: "v1",
+				},
+			},
+			expected: nil,
+			version:  "v1",
+		},
+		{
+			name:     "should get StatusError when invalid PlatformAdmin type",
+			obj:      &unstructured.Unstructured{},
+			expected: apierrors.NewBadRequest(fmt.Sprintf("expected a PlatformAdmin but got a %T", &unstructured.Unstructured{})),
+		},
+	}
+
+	handler := PlatformAdminHandler{Manifests: &config.Manifest{}}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := handler.Default(context.TODO(), tc.obj)
+			assert.Equal(t, tc.expected, err)
+			if err == nil {
+				assert.Equal(t, tc.version, tc.obj.(*v1alpha2.PlatformAdmin).Spec.Version)
+			}
+		})
+	}
+}
+
+func TestDefaultV2(t *testing.T) {
+	type testCase struct {
+		name     string
+		obj      runtime.Object
+		platform string
+	}
+
+	tests := []testCase{
+		{
+			name: "should get default platform when valid PlatformAdmin object with spec empty platform",
+			obj: &v1alpha2.PlatformAdmin{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v1alpha2.PlatformAdminSpec{
+					Platform: "",
+				},
+			},
+			platform: v1alpha2.PlatformAdminPlatformEdgeX,
+		},
+		{
+			name: "should get no error when valid PlatformAdmin object with spec platform is test",
+			obj: &v1alpha2.PlatformAdmin{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v1alpha2.PlatformAdminSpec{
+					Platform: "test",
+				},
+			},
+			platform: "test",
+		},
+	}
+
+	handler := PlatformAdminHandler{Manifests: &config.Manifest{}}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_ = handler.Default(context.TODO(), tc.obj)
+			assert.Equal(t, tc.platform, tc.obj.(*v1alpha2.PlatformAdmin).Spec.Platform)
+		})
+	}
+}
