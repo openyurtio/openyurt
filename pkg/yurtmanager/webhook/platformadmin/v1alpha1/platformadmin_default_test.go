c4032ad19d73df9c06d1d319d88d4ae4681ffb32
leo9527 <1831131300@qq.com>
2024-08-09 22:11:13 +0800

    feat: add platformadmin ut with tongyilingma

--- pkg/yurtmanager/webhook/platformadmin/v1alpha1/platformadmin_default_test.go
+++ pkg/yurtmanager/webhook/platformadmin/v1alpha1/platformadmin_default_test.go
@@ -1,31 +1,34 @@
-package v1alpha1
-
 /*
 Copyright 2023 The OpenYurt Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
+
+Licensed under the Apache License, Version 2.0 (the License);
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-	http://www.apache.org/licenses/LICENSE-2.0
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an AS IS BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+package v1alpha1
 
 import (
 	"context"
 	"fmt"
 	"testing"
 
-	"github.com/openyurtio/openyurt/pkg/apis/iot/v1alpha1"
-	"github.com/openyurtio/openyurt/pkg/yurtmanager/controller/platformadmin/config"
 	"github.com/stretchr/testify/assert"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/openyurtio/openyurt/pkg/apis/iot/v1alpha1"
+	"github.com/openyurtio/openyurt/pkg/yurtmanager/controller/platformadmin/config"
 )
 
 // TestDefault tests the Default method of PlatformAdminHandler.
