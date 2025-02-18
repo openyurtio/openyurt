db3197bf0125bf78fe364448f65a593e21a8fc83
tnsimon <s.tien@hotmail.com>
2025-02-18 16:16:40 +1100

    feat: add hub leader RBAC controller (#2328)
    Co-authored-by: Simon Tien <tiensimon@microsoft.com>

--- cmd/yurt-manager/names/controller_names.go
+++ cmd/yurt-manager/names/controller_names.go
@@ -37,6 +37,7 @@ const (
 	LoadBalancerSetController              = "load-balancer-set-controller"
 	HubLeaderController                    = "hubleader-controller"
 	HubLeaderConfigController              = "hubleaderconfig-controller"
+	HubLeaderRBACController                = "hubleaderrbac-controller"
 )
 
 func YurtManagerControllerAliases() map[string]string {
@@ -62,5 +63,6 @@ func YurtManagerControllerAliases() map[string]string {
 		"loadbalancerset":               LoadBalancerSetController,
 		"hubleader":                     HubLeaderController,
 		"hubleaderconfig":               HubLeaderConfigController,
+		"hubleaderrbac":                 HubLeaderRBACController,
 	}
 }
